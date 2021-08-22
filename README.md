cb228069dce25c1004b76bf2a291ef9437b001e8
Luis Vazquez <luisvazquezcontadorpublico@gmail.com>
2021-08-22 19:11:45 -0300

    se conecto a firebase para utilizar la database en tiempo real

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 ## APP-CONTACTOS
 ###  Projecto que simula una agenda de contactos
 
+# Resources
+
+[https://firebase.google.com/docs/admin/setup]
+[https://firebase.google.com/docs/firestore/manage-data/delete-data]
+
+
+
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,50 @@
 const { Router } = require('express');
 const router = Router();
-router.get('/', (req, res) => {
+const admin = require('firebase-admin');
+require('dotenv').config()
 
-    console.log('index works..!');
-    res.render('index');
+
+var serviceAccount = require(process.env.CREDENTIAL_GOOGLE_APPLICATION);
+
+//configuracion firebase
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://app-contactos-example-default-rtdb.firebaseio.com/'
 });
+
+const db = admin.database();
+
+//RUTAS
+router.get('/', (req, res) => {
+    db.ref('contacts').once('value', (snapshot) => {
+        const data = snapshot.val();
+        res.render('index', { contacts: data });
+    });
+
+});
+router.post('/new-contact', (req, res) => {
+    const { firstname, lastname, email, phone } = req.body;
+
+    const newContact = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        phone: phone
+    }
+    if (firstname && lastname && email && phone) {
+        db.ref('contacts').push(newContact)
+        res.redirect('/');
+    } else {
+        res.redirect('/');
+
+    }
+
+});
+
+router.get("/delete-contact/:id", (req, res) => {
+    db.ref('contacts/' + req.params.id).remove();
+    res.redirect('/')
+})
 
 
 
